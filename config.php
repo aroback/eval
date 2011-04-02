d2e0af1dc94d6541ed17e875ec5aa56df4ce2faf
Andrew Roback <andrew.roback@gmail.com>
2011-04-01 22:54:01 -0500

    post install commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysql';
+$dbhost = '';
+$dbport = '';
+$dbname = 'eval';
+$dbuser = 'phpbb';
+$dbpasswd = 'phpbb';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
