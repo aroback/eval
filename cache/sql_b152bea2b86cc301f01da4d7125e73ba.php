5810eda25ef5e26b7e6cf48abf9b844e4a82678d
Andrew Roback <andrewroback@dhcp112.sewn2.iit.edu>
2011-04-06 15:31:28 -0500

    added cache folder

---
+++ cache/sql_b152bea2b86cc301f01da4d7125e73ba.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1302122453
+SELECT forum_id, forum_name, parent_id, forum_type, left_id, right_id FROM phpbb_forums ORDER BY left_id ASC
+549
+a:3:{i:0;a:6:{s:8:"forum_id";s:1:"2";s:10:"forum_name";s:27:"Instructor Evaluation Forum";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}i:1;a:6:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:23:"Course Evaluation Forum";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"6";}i:2;a:6:{s:8:"forum_id";s:1:"4";s:10:"forum_name";s:31:"Course Strengths and Weaknesses";s:9:"parent_id";s:1:"3";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"4";s:8:"right_id";s:1:"5";}}
