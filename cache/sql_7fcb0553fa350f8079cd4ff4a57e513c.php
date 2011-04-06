ee61ec3bca40638022877503bd96f36662ddb351
Andrew Roback <andrewroback@dhcp112.sewn2.iit.edu>
2011-04-06 15:29:32 -0500

    deleted cache directory

--- cache/sql_7fcb0553fa350f8079cd4ff4a57e513c.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1301969705
-SELECT forum_name FROM phpbb_forums WHERE forum_id = 2
-69
-a:1:{i:0;a:1:{s:10:"forum_name";s:27:"Instructor Evaluation Forum";}}
--- cache/sql_b12dc59e5cedf058ff7a6e8e06baed03.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1301969592
-SELECT forum_id, forum_name FROM phpbb_forums WHERE forum_id IN ('2', '2')
-92
-a:1:{i:0;a:2:{s:8:"forum_id";s:1:"2";s:10:"forum_name";s:27:"Instructor Evaluation Forum";}}
