f5d245cac1fa428c7617010231f54cd98387fad6
Charise Angderson <charise.angderson@gmail.com>
2011-04-24 19:00:56 -0500

    make add user visible and e-mails used multiple times

---
+++ cache/sql_b152bea2b86cc301f01da4d7125e73ba.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1303689047
+SELECT forum_id, forum_name, parent_id, forum_type, left_id, right_id FROM phpbb_forums ORDER BY left_id ASC
+674
+a:4:{i:0;a:6:{s:8:"forum_id";s:1:"2";s:10:"forum_name";s:11:"Spring 2011";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}i:1;a:6:{s:8:"forum_id";s:1:"5";s:10:"forum_name";s:9:"Fall 2009";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"4";}i:2;a:6:{s:8:"forum_id";s:1:"6";s:10:"forum_name";s:6:"Alumni";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"5";s:8:"right_id";s:1:"6";}i:3;a:6:{s:8:"forum_id";s:1:"7";s:10:"forum_name";s:28:"Instructors, TAs, and Admins";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"7";s:8:"right_id";s:1:"8";}}
